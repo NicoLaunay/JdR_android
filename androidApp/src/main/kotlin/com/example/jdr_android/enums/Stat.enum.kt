8480e127547e693e44d27387e49b18c5a3165b3d
Nicolas <nicolas.launay.0@gmail.com>
2026-05-22 10:53:50 +0200

    NEW(stat.enum): added enum class for character stats

---
+++ androidApp/src/main/kotlin/com/example/jdr_android/enums/Stat.enum.kt
@@ -0,0 +1,10 @@
+package com.example.jdr_android.enums
+
+enum class Stat(val displayName: String, val trigram: String) {
+    STRENGTH("Force", "FOR"),
+    DEXTERITY("Dextérité", "DEX"),
+    CONSTITUTION("Constitution", "CON"),
+    INTELLIGENCE("Intelligence", "INT"),
+    WISDOM("Sagesse", "SAG"),
+    CHARISMA("Charisme", "CHA"),
+}
