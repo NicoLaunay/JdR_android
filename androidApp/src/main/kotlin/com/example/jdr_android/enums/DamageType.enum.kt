bd1b3dc6dfebcc1c43bfc88c1ec92e8887e7ed17
Nicolas <nicolas.launay.0@gmail.com>
2026-05-22 11:15:10 +0200

    NEW(DamageType.enum): added enum class for damage types

---
+++ androidApp/src/main/kotlin/com/example/jdr_android/enums/DamageType.enum.kt
@@ -0,0 +1,9 @@
+package com.example.jdr_android.enums
+
+enum class DamageType(val displayName: String) {
+    PHYSICAL("Physique"),
+    MAGICAL("Magique"),
+    FIRE("Feu"),
+    ICE("Glace"),
+    ACID("Acide")
+}
