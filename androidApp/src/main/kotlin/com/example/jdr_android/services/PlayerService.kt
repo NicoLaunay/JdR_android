bc02895c02c3cdda8b4da89cd16ea7eef926ab5c
Nicolas <nicolas.launay.0@gmail.com>
2026-05-22 14:15:20 +0200

    EDIT(PlayerService): added update method and implemented mapper

--- androidApp/src/main/kotlin/com/example/jdr_android/services/PlayerService.kt
+++ androidApp/src/main/kotlin/com/example/jdr_android/services/PlayerService.kt
@@ -1,6 +1,7 @@
 package com.example.jdr_android.services
 
 import com.example.jdr_android.db.JdrDatabase
+import com.example.jdr_android.mappers.toModel
 import com.example.jdr_android.models.Player
 
 class PlayerService(private val db: JdrDatabase) {
@@ -9,19 +10,22 @@ class PlayerService(private val db: JdrDatabase) {
         db.playerQueries
             .selectAll()
             .executeAsList()
-            .map {
-                Player(
-                id = it.id.toInt(),
-                name = it.name)
-            }  // SQLDelight → modèle
+            .map { it.toModel() }  // SQLDelight → modèle
 
-//    fun getById(id: Int): Player =
-//        db.playerQueries
-//            .selectById(id.toLong())
+    fun getById(id: Int): Player =
+        db.playerQueries
+            .selectById(id.toLong())
+            .executeAsOne()
+            .toModel()
 
     fun add(name: String) {
         require(name.isNotBlank()) { "Le nom ne peut pas être vide" }
         db.playerQueries.insert(name)
+    }
+
+    fun update(id: Int, name: String) {
+        require(name.isNotBlank()) { "Le nom ne peut pas être vide" }
+        db.playerQueries.updateById(name, id.toLong())
     }
 
     fun delete(id: Int) =
