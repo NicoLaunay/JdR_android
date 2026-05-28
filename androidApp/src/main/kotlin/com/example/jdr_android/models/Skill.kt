69d8e1d8790a435cbb1c91bcb7c0dfcdd4660ebf
Nicolas <nicolas.launay.0@gmail.com>
2026-05-28 16:27:05 +0200

    EDIT(Skill): added isLimited, isSpell and level columns, and updated model

--- androidApp/src/main/kotlin/com/example/jdr_android/models/Skill.kt
+++ androidApp/src/main/kotlin/com/example/jdr_android/models/Skill.kt
@@ -3,5 +3,8 @@ package com.example.jdr_android.models
 data class Skill(
     val id: Int,
     val name: String,
+    val level: Int,
     val description: String,
+    val isLimited: Boolean,
+    val isSpell: Boolean,
 )
